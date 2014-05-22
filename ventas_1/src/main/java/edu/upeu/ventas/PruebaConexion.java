14196867eab4157afafb4d027b4d2af6306aba22
vidalmamanijordan <vidal_mamani@upeu.edu.pe>
2014-05-21 23:44:46 -0500

    Modificando la conexion de la base de datos...

--- ventas_1/src/main/java/edu/upeu/ventas/PruebaConexion.java
+++ ventas_1/src/main/java/edu/upeu/ventas/PruebaConexion.java
@@ -2,56 +2,44 @@ package edu.upeu.ventas;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 public class PruebaConexion {
-
-	Connection conn = null;
-
-	public Connection getConexion() throws SQLException {
-
-		if (conn == null) {
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-				conn = DriverManager.getConnection(
-						"jdbc:mysql://localhost:3306/ventas", "root", "123456");
-				System.out.println("Conexion exitosa");
-			} catch (ClassNotFoundException e) {
-				System.out.println("Conexion fallida");
-				e.printStackTrace();
-			}
-		}
-
-		return conn;
-	}
-
-	public static void main(String[] args) {
-
-		PruebaConexion pc = new PruebaConexion();
-
-		try {
-			Connection c = pc.getConexion();
-
-			PreparedStatement ps = c
-					.prepareStatement("select * from usuario where username = ? and password = ?");
-			ps.setString(1, "admin1");
-			ps.setString(2, "admin");
-			ResultSet rs = ps.executeQuery();
-
-			if (rs.next()) {
-				System.out.println("Usuario : " + rs.getString("username"));
-				System.out.println("Contrasenia : " + rs.getString("password"));
-			} else {
-				System.out.println("No se encontraron resultados");
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-	}
-
+  public Connection cn=null;
+  public Statement st=null;
+  public ResultSet rs=null; 
+  
+  public Connection conectar() throws Exception{
+      try {
+          
+      String servidor="localhost";
+      String puerto="3306";
+      String usuario="ventas";
+      String password="123456";
+      String servicio="root";
+      String driver="com.mysql.jdbc.Driver";
+      String cadenaconexion="jdbc:mysql"+servidor+":"+puerto+":"+servicio;
+      
+      Class.forName(driver);      
+      cn=(Connection) DriverManager.getConnection(cadenaconexion,usuario,password);
+      System.out.println("la coneccion fue exitosa..."); 
+      
+      } catch (Exception e) {
+      System.out.println("herror de conexion "+e.getMessage());
+      }
+        return cn;
+  }
+    public ResultSet ejecutarConsulta(String sql) throws Exception{
+      st=(Statement) cn.createStatement();
+      rs=st.executeQuery(sql);
+      return rs;
+  }
+      public void ejecutarActualizacion(String sql) throws Exception{
+      st=(Statement) cn.createStatement();
+      st.executeUpdate(sql);
+  }
+     public void desconectar() throws Exception{
+      cn.close();
+  }   
 }
